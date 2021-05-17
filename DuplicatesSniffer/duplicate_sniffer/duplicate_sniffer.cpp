9e1510cf66b4ae4621b105d2f9883a6721ab4acd
stephen-pinto <stephen_pinto@outlook.com>
2021-05-17 17:22:00 +0530

    Added the duplicate sniffer tool

---
+++ DuplicatesSniffer/duplicate_sniffer/duplicate_sniffer.cpp
@@ -0,0 +1,12 @@
+﻿// duplicate_sniffer.cpp : Defines the entry point for the application.
+//
+
+#include "duplicate_sniffer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
